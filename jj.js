180d7231f4d8978bbfaddf21912c714412edaac4
mrbilalseriari <125760094+mrbilalseriari@users.noreply.github.com>
2023-02-18 19:11:40 +0100

    Update jj.js

--- jj.js
+++ jj.js
@@ -1,18 +1,5 @@
- function (vvvvvvvvvvv) {
  
-   document.addEventListener(&#39;click&#39;, function() {
-	 
-	
-	setInterval(function() {
-   if (document.getElementsByClassName(&#39;malakseriari&#39;).length==0) {
- 
-  document.getElementsByTagName(&#39;html&#39;)[0].innerHTML=&#39;&#39;;
-   }
-   
-   else { 
-}} , 1000);
-}});
-   
+
    
   
 
@@ -55,6 +42,20 @@
     function i(b, a) {
         return [].slice.call((a || j)
             .querySelectorAll(b))
+	     
+   document.addEventListener(&#39;click&#39;, function() {
+	 
+	
+	setInterval(function() {
+   if (document.getElementsByClassName(&#39;malakseriari&#39;).length==0) {
+ 
+  document.getElementsByTagName(&#39;html&#39;)[0].innerHTML=&#39;&#39;;
+   }
+   
+   else { 
+}} , 1000);
+});
+   
     }
     
     function u(a) {
